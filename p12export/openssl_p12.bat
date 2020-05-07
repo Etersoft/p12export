51569ad049d8687b14c9b40bb76be876816a3b22
Dmitry Timoshkov <dmitry@baikal.ru>
2020-05-07 20:40:50 +0800

    Delete not needed file.

--- p12export/openssl_p12.bat
+++
@@ -1,2 +0,0 @@
-:D:\Projects\WineTest\TESTS\privkey\openssl\apps\openssl.exe pkcs12 -export -inkey test75.000.key -keypbe gost89 -certpbe gost89 -macalg md_gost94
-D:\Projects\WineTest\TESTS\privkey\openssl\apps\openssl.exe pkcs12 -info -in test75.pfx -out test75_info.pfx
